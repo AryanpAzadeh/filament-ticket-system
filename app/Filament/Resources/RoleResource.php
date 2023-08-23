845b57c1387092647d8099a33b3b8a29c5d2ad76
AryaAzadeh <aryazdh@gmail.com>
2023-08-23 12:44:04 +0330

    PermissionsRelationManager
    add PermissionsRelationManager

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -59,7 +59,7 @@ class RoleResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\PermissionsRelationManager::class
         ];
     }
 
